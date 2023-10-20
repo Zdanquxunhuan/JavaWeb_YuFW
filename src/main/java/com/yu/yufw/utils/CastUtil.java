01415c03f8b2b407c404cbd2aa6f4e0d49cc9dec
zhongcanyu <zcyzcy159162>
2023-10-20 16:54:48 +0800

    feat:customer,castUtil,PropsUtil

---
+++ src/main/java/com/yu/yufw/utils/CastUtil.java
@@ -0,0 +1,12 @@
+package com.yu.yufw.utils;
+
+public class CastUtil {
+    public static int castInt(String property) {
+
+        return 1;
+    }
+
+    public static boolean castBoolean(String property) {
+        return false;
+    }
+}
