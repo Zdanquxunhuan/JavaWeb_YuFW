abbfbfda8f1afef6c15b1c78db4c9788adc7c900
zhongcanyu <zcyzcy159162>
2023-10-20 16:37:11 +0800

    init

---
+++ src/main/java/com/yu/yufw/JavaWebYuFwApplication.java
@@ -0,0 +1,13 @@
+package com.yu.yufw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebYuFwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebYuFwApplication.class, args);
+    }
+
+}
