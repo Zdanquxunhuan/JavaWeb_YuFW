085fed8997443b5b0c1e6088913e93aca8383c73
ZCY <982287281@qq.com>
2023-10-22 23:41:33 +0800

    some change

--- src/main/java/com/yu/yufw/JavaWebYuFwApplication.java
+++ src/main/java/com/yu/yufw/JavaWebYuFwApplication.java
@@ -1,13 +1,11 @@
 package com.yu.yufw;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class JavaWebYuFwApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaWebYuFwApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(JavaWebYuFwApplication.class, args);
+//    }
 
 }
